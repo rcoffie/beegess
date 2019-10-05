b6d8d000b71cfdcf5ced2368dfb4813f1324328e
Robert <rcoffie22@yahoo.com>
2019-10-05 10:19:26 +0000

    ''

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="utf-8">
   
   <link rel="stylesheet"  type="text/css"  href="css/uikit.min.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400&display=swap" rel="stylesheet"> 
--- includes/nav.php
+++ includes/nav.php
@@ -1,32 +1,53 @@
 <nav class="uk-navbar-container " uk-navbar>
-    <div class="uk-navbar-left">
+    <div class="uk-navbar-left ">
 
-        <a class="uk-navbar-item uk-logo" href="#">Logo</a>
+        <a class="uk-navbar-item uk-logo uk-visible@m" href="#">Logo</a>
 
-        <ul class="uk-navbar-nav">
-            <li>
-                <a href="#">
-                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
-                    Features
-                </a>
-            </li>
-        </ul>
+     
 
-        <div class="uk-navbar-item">
-            <div>Some <a href="#">Link</a></div>
+        <div class="uk-navbar-item uk-visible@m">
+            
             <a class="uk-button " href="#service" uk-scroll>Service</a>
             <a class="uk-button " href="#work" uk-scroll>Our Works</a>
             <a class="uk-button " href="#about" uk-scroll> About</a>
             <a class="uk-button " href="#contact" uk-scroll> Contact</a>
         </div>
+      
+        <a class="uk-button  uk-margin-small-right uk-hidden@m"  uk-toggle="target: #offcanvas-push"> <span uk-icon="menu"></span> </a>
+         
+       </div>
 
-        <div class="uk-navbar-item">
-            <form action="javascript:void(0)">
-                <input class="uk-input uk-form-width-small" type="text" placeholder="Input">
-                <button class="uk-button uk-button-default">Button</button>
-            </form>
-        </div>
+   
+</nav>
+
+
+
+
+
+
+
+<div id="offcanvas-push" uk-offcanvas="mode: push; overlay: true">
+
+
+       
+
+        <div class="uk-offcanvas-bar">
+        <button class="uk-offcanvas-close" type="button" uk-close></button>
+
+        <ul class="uk-nav uk-nav-offcanvas" data-uk-nav>
+           
+       
+            
+            <a class="uk-button " href="#service" uk-scroll>Service</a>
+            <a class="uk-button " href="#work" uk-scroll>Our Works</a>
+            <a class="uk-button " href="#about" uk-scroll> About</a>
+            <a class="uk-button " href="#contact" uk-scroll> Contact</a>
+          
+        </ul>
 
     </div>
-</nav>
-  
+
+       
+</div>
+
+
